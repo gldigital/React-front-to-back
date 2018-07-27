2bd9ea204f009931a92672aa4ccf2b18da48c609
Greg Ladden <greg@gl-digital.com>
2018-07-26 22:30:27 -0500

    adding contact and delete func

--- contact-manager/src/components/Contact.js
+++ contact-manager/src/components/Contact.js
@@ -1,33 +1,62 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import { Consumer } from "../context";
 
 class Contact extends Component {
 
-  state = {};
+  state = {
+    showContactInfo: false
+  };
 
-  onShowClick = () => {
-    console.log(this.state)
+  onShowClick = (e) => {
+    this.setState({ showContactInfo: !this.state.showContactInfo })
   }
+
+  onDeleteClick = (id, dispatch) => {
+    dispatch({
+      type: 'DELETE_CONTACT', payload: id
+    });
+  };
+
   render() {
-    const { name, email, phone } = this.props.contact;
+    const { id, name, email, phone } = this.props.contact;
+    const { showContactInfo } = this.state;
+
     return (
-      <div className="card card-body mb-3">
-        <h4>{name}
-          <i
-            onClick={this.onShowClick} className="fas fa-sort-down">
-          </i>
-        </h4>
-        <ul className="list-group">
-          <li className="list-group-item">{email}</li>
-          <li className="list-group-item">{phone}</li>
-        </ul>
-      </div>
+      <Consumer>
+        {value => {
+          const { dispatch } = value;
+          return (
+            <div className="card card-body mb-3">
+              <h4>{name}
+                <i
+                  onClick={this.onShowClick} className="fas fa-sort-down"
+                  style={{ cursor: 'pointer' }}
+                />
+                <i
+                  className="fas fa-times" style={{
+                    cursor: 'pointer', float: 'right',
+                    color: 'red'
+                  }}
+                  onClick={this.onDeleteClick.bind(this, id, dispatch)}
+                />
+              </h4>
+              {showContactInfo ? (
+                <ul className="list-group">
+                  <li className="list-group-item">{email}</li>
+                  <li className="list-group-item">{phone}</li>
+                </ul>
+              ) : null}
+            </div>
+          )
+        }}
+      </Consumer>
     )
   }
 }
 
 Contact.propTypes = {
-  contact: PropTypes.object.isRequired
+  contact: PropTypes.object.isRequired,
 }
 
 export default Contact;
--- contact-manager/src/components/Contacts.js
+++ contact-manager/src/components/Contacts.js
@@ -1,46 +1,27 @@
 import React, { Component } from 'react'
 import Contact from './Contact'
+import { Consumer } from '../context'
 
 class Contacts extends Component {
 
-  constructor() {
-    super();
-    this.state = {
-      contacts: [
-        {
-          id: 1,
-          name: 'Greg Ladden',
-          email: 'greg@gmail.com',
-          phone: '333-444-5555'
-        },
-        {
-          id: 2,
-          name: 'Erica Ladden',
-          email: 'Erica@gmail.com',
-          phone: '111-444-5555'
-        },
-        {
-          id: 3,
-          name: 'Dad Ladden',
-          email: 'dad@gmail.com',
-          phone: '222-444-5555'
-        }
-      ]
-    }
-  }
   render() {
-    const { contacts } = this.state;
-
     return (
-      <div>
-        {contacts.map(contact => (
-          <Contact
-            key={contact.id}
-            contact={contact}
-          />
-        ))}
-      </div>
-    )
+      <Consumer>
+        {value => {
+          const { contacts } = value;
+          return (
+            <React.Fragment>
+              {contacts.map(contact => (
+                <Contact
+                  key={contact.id}
+                  contact={contact}
+                />
+              ))}
+            </React.Fragment>
+          )
+        }}
+      </Consumer>
+    );
   }
 }
 
